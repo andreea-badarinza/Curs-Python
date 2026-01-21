6f0c61ce18d9cc9a62569954ad7de89d6e4089f0
andreea-badarinza <badarinzaandreea4@gmail.com>
2026-01-21 18:12:37 +0200

    Add files via upload

---
+++ L8/main_math.py
@@ -0,0 +1,10 @@
+# Importăm modulul creat de noi
+import math_operations
+
+a = 10
+b = 5
+
+print("Adunare:", math_operations.adunare(a, b))
+print("Scădere:", math_operations.scadere(a, b))
+print("Înmulțire:", math_operations.inmultire(a, b))
+print("Împărțire:", math_operations.impartire(a, b))
